8a80a97a72b5f20941f35b3fc3b22a3271c9e80b
krivonogov <zanax@yandex.ru>
2015-12-23 17:47:29 +0400

    23.12.2015-2

--- public/javascripts/libs/jquery.bootgrid.extend.js
+++ public/javascripts/libs/jquery.bootgrid.extend.js
@@ -1,10 +1,59 @@
 (function ($) {
 
+    function getRequest() {
+        var request = {
+                current: this.current,
+                rowCount: this.rowCount,
+                sort: this.sortDictionary,
+                searchPhrase: this.searchPhrase,
+                listType: this.listType
+            },
+            post = this.options.post;
+
+        post = ($.isFunction(post)) ? post() : post;
+        return this.options.requestHandler($.extend(true, request, post));
+    }
+
     var extensionMethods = {
 
-        sort: function () {
-            console.log(123);
+
+
+
+        append: function (rows, callback) {
+            var that = this;
+            if (this.options.ajax) {
+                $.ajax({
+                    method: 'PUT',
+                    data: {
+                        options: JSON.stringify({
+                            listType: that.listType,
+                            rows: rows
+                        })
+                    },
+                    url: '/grid'
+                }).done(function () {
+                    return callback();
+                }).fail(function (fail) {
+                    return callback(fail);
+                })
+            } else {
+                var appendedRows = [];
+                for (var i = 0; i < rows.length; i++) {
+                    if (appendRow.call(this, rows[i])) {
+                        appendedRows.push(rows[i]);
+                    }
+                }
+                sortRows.call(this);
+                highlightAppendedRows.call(this, appendedRows);
+                loadData.call(this);
+                this.element.trigger("appended" + namespace, [appendedRows]);
+            }
+
+            return this;
         }
+
+
+
     };
 
     $.extend($.fn.bootgrid.Constructor.prototype, extensionMethods)
